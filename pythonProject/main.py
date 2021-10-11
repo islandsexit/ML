e819f6a030e99dc6eafda265e0fe0aabdfc28b3b
islandsexit <hezkydoc@gmail.ru>
2021-10-11 16:11:22 +0300

    new

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,23 +1,34 @@
 import pandas as pd
 import json
 from geopy.geocoders import Nominatim
-from datetime import date, datetime
-
+from datetime import datetime
+import geocoder
 
 def read():
-    try:
-        gibddid = input('Введите id происшествия, если есть')
-        region = input('Введите region')
-        parent_region = input('Введите parent region')
-        date = datetime.today()
-        address = input("Введите адресс")
-        geolocator = Nominatim(user_agent="http")
-        location = geolocator.geocode(address)
-        df = pd.DataFrame([[gibddid, region, parent_region, date, address, location.latitude, location.longitude]],
-                          columns=['gibddid', 'region', 'parent_region', 'date', 'address', 'lat', 'lon'])
+
+        g = geocoder.location(input('Где это произошло?'))
+        print("Вы знаете точные координаты")
+        answer = input()
+        if answer == "Да":
+                ...
+        print("Это сегодня произошло? ")
+        answer = input()
+        if answer == "Да":
+            date = datetime.today()
+        else:
+            date = datetime.strptime(input("Введи дату в формате 18.08.21 18:00"), '%d.%m.%y %H:%M')
+        gibddid = input('Введите id происшествия, если есть '  )
+        region = input('Введите region ')
+        parent_region = input('Введите parent region ')
+
+        feat = input("Опишите ДТП (Что находилось рядом, освещение, состояние дороги)")
+        inj = input('Сколько пострадавших? Ответ в числовом формате')
+        dead = input('Сколько человек погибло? Ответ в числовом формате')
+
+        df = pd.DataFrame([[gibddid, region, parent_region, date, g.location, g.latitudeg.latitude, g.longitudeg.longitude, feat, inj,dead]],
+                          columns=['gibddid', 'region', 'parent_region', 'date', 'address', 'lat', 'lon', 'feat', 'inj_count', 'dead_count'])
         print(df)
-    except:
-        print('cheto')
+
 
 
 if __name__ == '__main__':
